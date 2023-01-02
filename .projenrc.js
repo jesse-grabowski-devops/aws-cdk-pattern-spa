183b68f83390661916e3d79c3ff6fff59083a9fc
Jesse Grabowski <github@jessegrabowski.com>
2023-01-02 17:00:32 -0500

    feat: use dependabot

--- .projenrc.js
+++ .projenrc.js
@@ -15,6 +15,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   gitignore: [
     '.idea',
   ],
+  dependabot: true,
+  depsUpgrade: false,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
