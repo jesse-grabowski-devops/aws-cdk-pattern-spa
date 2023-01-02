73f9c1170549e862c7140ace9c496919a49c4899
Jesse Grabowski <github@jessegrabowski.com>
2023-01-02 12:12:06 -0500

    feat: Add npm public

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,17 @@
 const { awscdk } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Jesse Grabowski',
-  authorAddress: 'github@jessegrabowski.com',
+  authorAddress: 'npm@jessegrabowski.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  name: 'aws-cdk-pattern-spa',
+  name: '@npm-jessegrabowski/aws-cdk-pattern-spa',
   repositoryUrl: 'git@github.com:jesse-grabowski-devops/aws-cdk-pattern-spa.git',
-
+  npmAccess: NpmAccess.PUBLIC,
+  gitignore: [
+    '.idea',
+  ],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
