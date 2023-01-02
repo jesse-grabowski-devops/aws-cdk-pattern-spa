3be6d89df4f2ae02b8578f8b64c253b5ce3bfd5f
Jesse Grabowski <github@jessegrabowski.com>
2023-01-02 14:35:32 -0500

    fix: fix build error

--- src/index.ts
+++ src/index.ts
@@ -87,12 +87,12 @@ export class SinglePageApplication extends Construct {
       defaultRootObject: 'index.html',
     });
 
-    const aRecord = new aws_route53.ARecord(this, 'aRecord', {
+    new aws_route53.ARecord(this, 'aRecord', {
       zone: hostedZone,
       recordName: domainName,
       target: aws_route53.RecordTarget.fromAlias(new aws_route53_targets.CloudFrontTarget(distribution)),
     });
-    const aaaaRecord = new aws_route53.AaaaRecord(this, 'aaaaRecord', {
+    new aws_route53.AaaaRecord(this, 'aaaaRecord', {
       zone: hostedZone,
       recordName: domainName,
       target: cdk.aws_route53.RecordTarget.fromAlias(new aws_route53_targets.CloudFrontTarget(distribution)),
